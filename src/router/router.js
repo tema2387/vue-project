0e3423679a70a6a69e60ef82dbcd9e177b96bf71
Artem <artembaranov.00@mail.ru>
2024-10-09 14:48:56 +0300

    add HelpPage

--- src/router/router.js
+++ src/router/router.js
@@ -5,12 +5,14 @@ import MainPage from '@/pages/front/MainPage.vue';
 import OrderPage from '@/pages/front/OrderPage.vue';
 import PlansPage from '@/pages/front/PlansPage.vue';
 import HelpPage from '@/pages/front/HelpPage.vue';
+import QuestionPage from '@/pages/front/QuestionPage.vue';
 
 const routes = [
   { path: '/', component: MainPage },
   { path: '/order', component: OrderPage },
   { path: '/plans', component: PlansPage },
-  { path: '/help', component: HelpPage }
+  { path: '/help', component: HelpPage },
+  { path: '/question/:id', component: QuestionPage },
 ]
 
 const router = createRouter({
